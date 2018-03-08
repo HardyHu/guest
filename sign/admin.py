310a4555c98a5fb11312195f2b38627e5ab98415
HardyHu <huzhikai11@sina.com>
2018-03-08 17:12:06 +0800

    update by HardyHu20180308
    I think emmm... there may lost some columns,and i copy what i write here,just hope it work!

--- sign/admin.py
+++ sign/admin.py
@@ -1,20 +1,18 @@
+# -*- coding: utf-8 -*-
 from django.contrib import admin
 from sign.models import *
 
-
 # Register your models here.
 class EventAdmin(admin.ModelAdmin):
-    list_display = ['name', 'status', 'start_time','id']
-    search_fields = ['name']    # 搜索功能
-    list_filter = ['status']    # 过滤器
-
+	list_dispaly = ['id','name','status','address','start_time']
+	search_fields = ['name']  #搜索栏
+	list_filter = ['status']  #过滤器
 
 class GuestAdmin(admin.ModelAdmin):
-    list_display = ['realname', 'phone','email','sign','create_time','event_id']
-    list_display_links = ('realname', 'phone') # 显示链接
-    search_fields = ['realname','phone']       # 搜索功能
-    list_filter = ['event_id']                 # 过滤器
+	list_dispaly = ['realname','phone','email','sign','create_time','event']
+    list_display_links = ('realname','phone')
+	search_fields = ['realname','phone']  #搜索栏
+	list_filter = ['sign']                #过滤器
 
-
-admin.site.register(Event, EventAdmin)
-admin.site.register(Guest, GuestAdmin)
+admin.site.register(Event,EventAdmin)
+admin.site.register(Guest,GuestAdmin)
